37098c4093cc7e6d7994c5fdc2e62c3c01e607da
johnasblasco <johnaslblasco@gmail.com>
2024-07-27 15:09:00 +0800

    web saket

--- server/index.js
+++ server/index.js
@@ -1,10 +1,22 @@
-const ws = require('ws');
-const server = new ws.Server({port: '8080' })
+import { createServer } from "http"
+import { Server } from "socket.io"
 
-server.on('connection', socket => {
-      socket.on('message', message => {
-            const b = Buffer.from(message)
-            console.log(b.toString())
-            socket.send(`${message}`)
-      })
+
+const httpServer = createServer()
+
+const io = new Server(httpServer, {
+    cors: {
+        origin: process.env.NODE_ENV === "production" ? false : ["http://localhost:5500", "http://127.0.0.1:5500"]
+    }
 })
+
+io.on('connection', socket => {
+    console.log(`User ${socket.id} connected`)
+
+    socket.on('message', data => {
+        console.log(data)
+        io.emit('message', `${socket.id.substring(0, 5)}: ${data}`)
+    })
+})
+
+httpServer.listen(3500, () => console.log('listening on port 3500'))
